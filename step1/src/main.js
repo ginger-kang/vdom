455721fa51cc6fa8a77de3899d6fcfa5fb359260
ginger-kang <kdhoon07@gmail.com>
2021-09-18 00:13:23 +0900

    Feat: add state

--- step1/src/main.js
+++ step1/src/main.js
@@ -7,17 +7,33 @@ function createElement(type, props = {}, ...children) {
   return { type, props, children };
 }
 
-function App() {
-  return (
-    <div id="app">
-      <h1>Hello</h1>
-      <ul>
-        <li className="item">shinkai</li>
-        <li className="item">smile</li>
-        <li className="item">kaerunouta</li>
-      </ul>
-    </div>
-  );
-}
+const todoList = [
+  { id: 1, checked: false, content: "todo item1" },
+  { id: 2, checked: true, content: "todo item2" },
+  { id: 3, checked: true, content: "todo item3" },
+];
 
-console.log(<App />);
+const vm = (
+  <div id="app">
+    <h1>Hello</h1>
+    <ul>
+      {todoList.map(({ checked, content }) => (
+        <li className={checked ? "checked" : null}>
+          <input type="checkbox" className="toggle" checked={checked} />
+          {content}
+          <button className="remove">삭제</button>
+        </li>
+      ))}
+    </ul>
+    <form>
+      <input type="text" />
+      <button type="submit">추가</button>
+    </form>
+  </div>
+);
+
+const $root = document.body.querySelector("#root");
+
+$root.innerHTML = `
+  <pre>${JSON.stringify(vm, null, 2)}</pre>
+`;
