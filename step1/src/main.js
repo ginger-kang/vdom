c01a0c6cdb9a3748774cac33b5a6843d1caec1dd
ginger-kang <kdhoon07@gmail.com>
2021-09-17 22:07:36 +0900

    Feat: function type exception

--- step1/src/main.js
+++ step1/src/main.js
@@ -1,20 +1,23 @@
 /** @jsx createElement */
 
 function createElement(type, props = {}, ...children) {
+  if (typeof type === "function") {
+    return type.apply(null, [props, ...children]);
+  }
   return { type, props, children };
 }
 
-const dom = (
-  <div id="app">
-    <h1>Hello</h1>
-    <ul>
-      <li className="item">shinkai</li>
-      <li className="item">smile</li>
-      <li className="item">kaerunouta</li>
-    </ul>
-  </div>
-);
+function App() {
+  return (
+    <div id="app">
+      <h1>Hello</h1>
+      <ul>
+        <li className="item">shinkai</li>
+        <li className="item">smile</li>
+        <li className="item">kaerunouta</li>
+      </ul>
+    </div>
+  );
+}
 
-const $root = document.querySelector("#root");
-
-$root.innerHTML = `<pre>${JSON.stringify(dom, null, 2)}</pre>`;
+console.log(<App />);
