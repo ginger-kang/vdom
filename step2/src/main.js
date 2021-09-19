bac4c875576aef32896b5026f481765ae2e78d1e
ginger-kang <kdhoon07@gmail.com>
2021-09-20 02:55:44 +0900

    Feat: render element

--- step2/src/main.js
+++ step2/src/main.js
@@ -4,7 +4,34 @@ function createElement(type, props = {}, ...children) {
   if (typeof type === "function") {
     return type.apply(null, [props, ...children]);
   }
-  return { type, props, children };
+  return { type, props, children: children.flat() };
+}
+
+function renderElement(node) {
+  if (typeof node === "string") {
+    return document.createTextNode(node);
+  }
+
+  const el = document.createElement(node.type);
+
+  Object.entries(node.props || {})
+    .filter(([attr, value]) => value)
+    .forEach(([attr, value]) => el.setAttribute(attr, value));
+
+  try {
+    node.children.map(renderElement).forEach(element => {
+      el.appendChild(element);
+    });
+  } catch (e) {
+    console.log(node);
+    console.log(e);
+  }
+
+  return el;
+}
+
+function render(vdom, container) {
+  container.appendChild(renderElement(vdom));
 }
 
 const todoList = [
@@ -13,27 +40,25 @@ const todoList = [
   { id: 3, checked: true, content: "todo item3" },
 ];
 
-const vm = (
-  <div id="app">
-    <h1>Hello</h1>
-    <ul>
-      {todoList.map(({ checked, content }) => (
-        <li className={checked ? "checked" : null}>
-          <input type="checkbox" className="toggle" checked={checked} />
-          {content}
-          <button className="remove">삭제</button>
-        </li>
-      ))}
-    </ul>
-    <form>
-      <input type="text" />
-      <button type="submit">추가</button>
-    </form>
-  </div>
-);
+function App() {
+  return (
+    <div id="app">
+      <h1>Hello</h1>
+      <ul>
+        {todoList.map(({ checked, content }) => (
+          <li className={checked ? "checked" : null}>
+            <input type="checkbox" className="toggle" checked={checked} />
+            {content}
+            <button className="remove">삭제</button>
+          </li>
+        ))}
+      </ul>
+      <form>
+        <input type="text" />
+        <button type="submit">추가</button>
+      </form>
+    </div>
+  );
+}
 
-const $root = document.body.querySelector("#root");
-
-$root.innerHTML = `
-  <pre>${JSON.stringify(vm, null, 2)}</pre>
-`;
+render(<App />, document.getElementById("root"));
